1f92f2856b1cbd9215b73b97cdd8f9850d594670
Shervin-2005 <shervinshams40@gmail.com>
2026-07-18 19:03:13 +0330

    write interfaces(unit of work, generic CRUD, simple product)

---
+++ Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        Task<IEnumerable<Product>> GetByCategoryAsync(int categoryId);
+        Task<Product?> GetWithDetailsAsync(int id);
+    }
+}
---
+++ Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IProductRepository Products { get; }
+        Task<int> SaveChangesAsync();
+    }
+}
