8b6d109770c5d924eea9093ce4352ed5abae60d9
Shervin-2005 <shervinshams40@gmail.com>
2026-07-26 16:39:17 +0330

    migration and database update

---
+++ Infastructure/Migrations/20260726130105_floderIdAdded.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class floderIdAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageFolderId",
+                table: "Categories",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageFolderId",
+                table: "Brands",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageFolderId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "ImageFolderId",
+                table: "Brands");
+        }
+    }
+}
