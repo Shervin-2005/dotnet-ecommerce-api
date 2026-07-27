a0dbf2e3feee47314b69f1f156b83aa11ec70036
Shervin-2005 <shervinshams40@gmail.com>
2026-07-27 17:06:57 +0330

    Add Automapper api key in user secrets

--- dotnet-ecommerce-api/Program.cs
+++ dotnet-ecommerce-api/Program.cs
@@ -24,12 +24,12 @@ builder.Services.AddScoped<IBrandService, BrandService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IImageStorageService, S3ImageStorageService>();
 
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 builder.Services.AddAutoMapper(cfg =>
 {
-    cfg.LicenseKey = "eyJhbGciOiJSUzI1NiIsImtpZCI6Ikx1Y2t5UGVubnlTb2Z0d2FyZUxpY2Vuc2VLZXkvYmJiMTNhY2I1OTkwNGQ4OWI0Y2IxYzg1ZjA4OGNjZjkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2x1Y2t5cGVubnlzb2Z0d2FyZS5jb20iLCJhdWQiOiJMdWNreVBlbm55U29mdHdhcmUiLCJleHAiOiIxODE2NTYwMDAwIiwiaWF0IjoiMTc4NTA3MTI2NiIsImFjY291bnRfaWQiOiIwMTlmOWU4Nzg3YjI3YTk3OGY3ZDU2OGU2MjY3YjM5OSIsImN1c3RvbWVyX2lkIjoiMDE5ZjllODc4N2IyN2E5NzhmN2Q1NjhlNjI2N2IzOTkiLCJzdWJfaWQiOiItIiwiZWRpdGlvbiI6IjAiLCJ0eXBlIjoiMiJ9.FQe68VIkVfMM91Oyw2TAcHGHh3mMdlbk6nSh_BRIoXYTLsCKgHB24_51_KnUPLU7cdwkkJplu8k5qQkgNPRqu6oXoYJ4d_lL-_j2K41fmCG_aMWKcXfuQ7hDYqEFD1xGNhEV7EQVrCiBr7DBxN93jfkyq8q7-IxaBUGNe8MHtHWaMDcVBiIVqlXkPCB2SeqkQmrtClwveKe4Sivixe91bB6YHNAxRNg3j3Q0iTF-KgueXSxdmDpsWvmrts_xMMkbZ1Mzr4LoN6HCBtXhMwi-_jmMmXi4baLLyk3kndYm_6nDJ-_UvOWYCb8GkLrp1yg0WpJbaNp7ZnqIX4WwcIWHbw";
+    cfg.LicenseKey = builder.Configuration["AutoMapper:ServiceApiKey"];
 }, typeof(MappingProfile));
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.Configure<S3Settings>(
     builder.Configuration.GetSection("ArvanStorage")
@@ -43,9 +43,6 @@ builder.Services.AddSingleton<IAmazonS3>(sp =>
 {
     var settings = sp.GetRequiredService<
         Microsoft.Extensions.Options.IOptions<S3Settings>>().Value;
-    Console.WriteLine($"ServiceUrl: '{settings.ServiceUrl}'");
-    Console.WriteLine($"AccessKey: '{settings.AccessKey}'");
-    Console.WriteLine($"BucketName: '{settings.BucketName}'");
     var credentials = new Amazon.Runtime.BasicAWSCredentials(
         settings.AccessKey,
         settings.SecretKey);
