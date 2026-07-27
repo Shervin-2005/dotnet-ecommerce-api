0e15ac01863731eff665856af917d193e3176c22
Shervin-2005 <shervinshams40@gmail.com>
2026-07-27 17:43:46 +0330

    Add user entity and it's configuration

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PasswordHash { get; set; }
+        public string PhoneNumber { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ Infastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infastructure.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.UserId);
+
+            builder.Property(u => u.FirstName)
+            .HasMaxLength(100);
+
+            builder.Property(u => u.LastName)
+                .HasMaxLength(100);
+
+            builder.Property(u => u.PhoneNumber)
+            .HasMaxLength(20);
+
+            builder.HasIndex(u => u.PhoneNumber)
+            .IsUnique();
+
+            builder.Property(u => u.CreatedAt)
+            .HasDefaultValueSql("GETUTCDATE()");
+        }
+    }
+}
