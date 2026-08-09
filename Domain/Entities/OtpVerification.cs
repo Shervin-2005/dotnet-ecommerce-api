3f95a35e675c218cd3c0537422bb70b7f333f0f3
Shervin-2005 <shervinshams40@gmail.com>
2026-08-09 19:38:20 +0330

    add OtpVerification entity with it configuration

---
+++ Domain/Entities/OtpVerification.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class OtpVerification
+    {
+        public int OtpVerificationId { get; set; }
+        public string PhoneNumber { get; set; } = null!;
+        public string CodeHash { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+        public bool IsUsed { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Infastructure/Configurations/OtpVerificationConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infastructure.Configurations
+{
+    public class OtpVerificationConfiguration : IEntityTypeConfiguration<OtpVerification>
+    {
+        public void Configure(EntityTypeBuilder<OtpVerification> builder)
+        {
+            builder.HasKey(o => o.OtpVerificationId);
+
+            builder.Property(o => o.PhoneNumber)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            builder.Property(o => o.CodeHash)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(o => o.ExpiresAt)
+                .IsRequired();
+
+            builder.Property(o => o.IsUsed)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            builder.Property(o => o.CreatedAt)
+                .IsRequired()
+                .HasDefaultValueSql("GETUTCDATE()");
+
+            builder.HasIndex(o => o.PhoneNumber);
+        }
+    }
+}
