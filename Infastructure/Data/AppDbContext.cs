9338cb829c75138051f30e1c9f384119cd3e0637
Shervin-2005 <shervinshams40@gmail.com>
2026-08-13 22:17:24 +0330

    configure entitires(user and otp) in AppDbContext

--- Infastructure/Data/AppDbContext.cs
+++ Infastructure/Data/AppDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Persistence
+namespace Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
--- Infastructure/Data/Configurations/OtpVerificationConfiguration.cs
+++ Infastructure/Data/Configurations/OtpVerificationConfiguration.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Infastructure.Configurations
+namespace Infastructure.Data.Configurations
 {
     public class OtpVerificationConfiguration : IEntityTypeConfiguration<OtpVerification>
     {
