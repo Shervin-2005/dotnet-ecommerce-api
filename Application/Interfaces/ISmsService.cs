bedf5edfe51345789357ff864ef2b9873acc0d03
Shervin-2005 <shervinshams40@gmail.com>
2026-08-17 17:46:22 +0330

    Write initial  SmsService

---
+++ Application/Interfaces/ISmsService.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Interfaces
+{
+    public interface ISmsService
+    {
+        Task SendAsync(string phoneNumber, string message);
+    }
+}
---
+++ Infastructure/Services/SmsService.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Services
+{
+    public class SmsService : ISmsService
+    {
+        private readonly ILogger<SmsService> _logger;
+
+        public SmsService(ILogger<SmsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task SendAsync(string phoneNumber, string message)
+        {
+            _logger.LogInformation("[SMS to {PhoneNumber}]: {Message}", phoneNumber, message);
+            return Task.CompletedTask;
+        }
+    }
+}
