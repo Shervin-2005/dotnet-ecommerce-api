0d42dd26be306229069e5c490f20e80d75cfb89c
Shervin-2005 <shervinshams40@gmail.com>
2026-08-17 17:26:05 +0330

    write user service interface

---
+++ Application/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Application.DTOs.Auth;
+
+namespace Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserDto?> GetByIdAsync(int userId);
+        Task<bool> UpdateProfileAsync(int userId, UpdateUserDto dto);
+        Task<bool> SoftDeleteAsync(int userId);
+    }
+}
