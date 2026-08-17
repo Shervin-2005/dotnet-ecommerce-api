cdd9c852cf7210ec948bf9d0f66ed8a1f6201568
Shervin-2005 <shervinshams40@gmail.com>
2026-08-17 17:46:01 +0330

    Wrtie PasswordHasherService

---
+++ Application/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+        bool Verify(string hashedPassword, string providedPassword);
+
+    }
+}
---
+++ Infastructure/Services/PasswordHasherService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Services
+{
+    public class PasswordHasherService : IPasswordHasher
+    {
+        private readonly PasswordHasher<User> _hasher = new();
+
+        public string Hash(string password) =>
+            _hasher.HashPassword(null!, password);
+
+        public bool Verify(string hashedPassword, string providedPassword) =>
+            _hasher.VerifyHashedPassword(null!, hashedPassword, providedPassword) != PasswordVerificationResult.Failed;
+    }
+}
