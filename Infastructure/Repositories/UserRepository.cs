54d49b177961175c57769e622a5534ccae9dccf1
Shervin-2005 <shervinshams40@gmail.com>
2026-08-17 17:25:48 +0330

    write user repository

---
+++ Infastructure/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context) : base(context)
+        {
+
+        }
+        public async Task<User?> GetByPhoneNumberAsync(string phoneNumber) =>
+            await _dbSet.FirstOrDefaultAsync(u => u.PhoneNumber == phoneNumber);
+    }
+}
