019c6d1471d29358c77dfaf0fd933b792ae0f304
Shervin-2005 <shervinshams40@gmail.com>
2026-08-17 17:25:31 +0330

    write user repository interface

---
+++ Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User?> GetByPhoneNumberAsync(string phoneNumber);
+    }
+}
