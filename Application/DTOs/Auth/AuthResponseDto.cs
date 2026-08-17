bbeb2d4f25dac442ab0cb9de2bfe542d228a2309
Shervin-2005 <shervinshams40@gmail.com>
2026-08-17 17:24:16 +0330

    write Auth and user DTOs

--- Application/DTOs/Auth/AuthResponseDto.cs
+++ Application/DTOs/Auth/AuthResponseDto.cs
@@ -1,16 +1,14 @@
-﻿
-using Domain.Enums;
+﻿using Domain.Enums;
 
 namespace Application.DTOs.Auth
 {
-    public class UserDto
+    public class AuthResponseDto
     {
+        public string Token { get; set; } = null!;
         public int UserId { get; set; }
+        public string PhoneNumber { get; set; } = null!;
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string PhoneNumber { get; set; } = null!;
         public UserRole Role { get; set; }
-        public bool HashPassword { get; set; }
-        public DateTime CreatedAt { get; set; }
     }
 }
--- Application/DTOs/Auth/UpdateUserDto.cs
+++ Application/DTOs/Auth/UpdateUserDto.cs
@@ -1,10 +1,15 @@
-﻿namespace Application.DTOs.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Auth
 {
     public class UpdateUserDto
     {
+        [StringLength(100)]
         public string? FirstName { get; set; }
+        [StringLength(100)]
         public string? LastName { get; set; }
 
+        [MinLength(8), MaxLength(12)]
         public string? Password { get; set; }
     }
 }
