bd3091a30c7d916428797d557e781b4dc8c9a1dc
Shervin-2005 <shervinshams40@gmail.com>
2026-08-17 17:41:26 +0330

    write AuthService interface

---
+++ Application/Interfaces/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs.Auth;
+
+namespace Application.Interfaces
+{
+    public interface IAuthService
+    {
+        Task RequestRegistrationOtpAsync(RequestOtpDto dto);
+        Task<AuthResponseDto> VerifyRegistrationOtpAsync(VerifyRegistrationOtpDto dto);
+        Task RequestLoginOtpAsync(RequestOtpDto dto);
+        Task<AuthResponseDto?> VerifyLoginWithOtpAsync(LoginWithOtpDto dto);
+        Task<AuthResponseDto?> LoginWithPasswordAsync(LoginWithPasswordDto dto);
+    }
+}
