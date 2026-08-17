e602c33fa59da60db7cbb85da03f7a8e356180ab
Shervin-2005 <shervinshams40@gmail.com>
2026-08-17 17:50:27 +0330

    write user controller

---
+++ dotnet-ecommerce-api/Controller/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Application.DTOs.Auth;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace dotnet_ecommerce_api.Controller
+{
+    [Authorize]
+    public class UserController : BaseController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet("me")]
+        public async Task<ActionResult<UserDto>> GetMe()
+        {
+            var user = await _userService.GetByIdAsync(GetUserId());
+            if (user is null) return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPut("me")]
+        public async Task<IActionResult> UpdateMe(UpdateUserDto dto)
+        {
+            var updated = await _userService.UpdateProfileAsync(GetUserId(), dto);
+            if (!updated) return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _userService.SoftDeleteAsync(id);
+            if (!deleted) return NotFound();
+            return NoContent();
+        }
+
+        private int GetUserId()
+        {
+            var value = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (!int.TryParse(value, out var userId))
+                throw new UnauthorizedAccessException("Invalid user identity.");
+
+            return userId;
+        }
+    }
+}
+
