e429e58f20c541baa2baa2bbb0e94bf2a62a6444
Shervin-2005 <shervinshams40@gmail.com>
2026-08-18 19:44:58 +0330

    Write password Dto and Request model

---
+++ Application/DTOs/Auth/AddPasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs.Auth
+{
+    public class AddPasswordDto
+    {
+        public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+        public string Otp { get; set; } = string.Empty;
+    }
+}
---
+++ dotnet-ecommerce-api/Models/ChangePasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_ecommerce_api.Models
+{
+    public class ChangePasswordRequest
+    {
+        public string CurrentPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
