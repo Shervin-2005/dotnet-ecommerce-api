74b90ab3b8b5361eae80ba298e685ca70384fcca
Shervin-2005 <shervinshams40@gmail.com>
2026-08-18 19:48:23 +0330

    add two enum for password process result

---
+++ Domain/Enums/ChangePasswordResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums
+{
+    public enum ChangePasswordResult
+    {
+        Success,
+        UserNotFound,
+        IncorrectCurrentPassword,
+        CurrentPasswordNotFound
+    }
+}
