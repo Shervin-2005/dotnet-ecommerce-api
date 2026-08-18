e787c71c8fcafb4e62a40d3f29c39bc6bd8105b7
Shervin-2005 <shervinshams40@gmail.com>
2026-08-18 16:28:07 +0330

    Write model UserRequest

---
+++ dotnet-ecommerce-api/Models/UserRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_ecommerce_api.Models
+{
+    public class UserRequest
+    {
+        [StringLength(100)]
+        public string? FirstName { get; set; }
+        [StringLength(100)]
+        public string? LastName { get; set; }
+
+        [Required]
+        public IFormFile File { get; set; } = null!;
+    }
+}
