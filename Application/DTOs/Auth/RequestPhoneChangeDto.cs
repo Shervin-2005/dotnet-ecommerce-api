1409843b8829103401e2da25a1cf258cbbfe4f8d
Shervin-2005 <shervinshams40@gmail.com>
2026-08-18 21:40:53 +0330

    Add DTOs corespond to changing phone number

---
+++ Application/DTOs/Auth/RequestPhoneChangeDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Auth
+{
+    public class RequestPhoneChangeDto
+    {
+        [Required, Phone]
+        public string NewPhoneNumber { get; set; } = null!;
+    }
+}
---
+++ Application/DTOs/Auth/VerifyPhoneChangeDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.Auth
+{
+    public class VerifyPhoneChangeDto
+    {
+        [Required, Phone]
+        public string NewPhoneNumber { get; set; } = null!;
+
+        [Required, StringLength(6, MinimumLength = 6)]
+        public string Code { get; set; } = null!;
+    }
+}
