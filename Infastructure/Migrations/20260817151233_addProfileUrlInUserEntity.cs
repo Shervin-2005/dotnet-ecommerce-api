ab9303eaa83470fcd0eac53e078658fd5b2f5c92
Shervin-2005 <shervinshams40@gmail.com>
2026-08-18 16:31:09 +0330

    Add migration and Update DB

---
+++ Infastructure/Migrations/20260817151233_addProfileUrlInUserEntity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProfileUrlInUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageFolderId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileUrl",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageFolderId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileUrl",
+                table: "Users");
+        }
+    }
+}
