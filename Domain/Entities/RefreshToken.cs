10e5004ca5deca70db76201ca9048ccca918f36f
Shervin-2005 <shervinshams40@gmail.com>
2026-08-20 12:14:04 +0330

    Write RefreshToken Entity

---
+++ Domain/Entities/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public class RefreshToken
+    {
+        public int RefreshTokenId { get; set; }
+        public int UserId { get; set; }
+        public string TokenHash { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime? RevokedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public User User { get; set; } = null!;
+
+        public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+    }
+}
