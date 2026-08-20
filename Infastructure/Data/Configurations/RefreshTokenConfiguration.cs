28573c046c572d0826302f57e41a528579ea3506
Shervin-2005 <shervinshams40@gmail.com>
2026-08-20 12:15:45 +0330

    Configure RefreshToken Fields

---
+++ Infastructure/Data/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.HasKey(t => t.RefreshTokenId);
+
+            builder.Property(t => t.TokenHash)
+                .IsRequired()
+                .HasMaxLength(128);
+
+            builder.HasIndex(t => t.TokenHash)
+                .IsUnique();
+
+            builder.HasOne(t => t.User)
+                .WithMany()
+                .HasForeignKey(t => t.UserId)
+                .OnDelete(DeleteBehavior.Cascade); 
+        }
+    }
+}
