4c6b80dbbd50bea5dde3339211772b7731ddeb83
Shervin-2005 <shervinshams40@gmail.com>
2026-08-20 12:17:53 +0330

    Write and implement RefreshToken Repo

---
+++ Application/Interfaces/IRefreshTokenRepository .cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IRefreshTokenRepository : IGenericRepository<RefreshToken>
+    {
+        Task<RefreshToken?> GetByTokenHashAsync(string tokenHash);
+    }
+}
---
+++ Infastructure/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class RefreshTokenRepository : GenericRepository<RefreshToken>, IRefreshTokenRepository
+    {
+        public RefreshTokenRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<RefreshToken?> GetByTokenHashAsync(string tokenHash) =>
+            await _dbSet.FirstOrDefaultAsync(t => t.TokenHash == tokenHash);
+
+    }
+}
