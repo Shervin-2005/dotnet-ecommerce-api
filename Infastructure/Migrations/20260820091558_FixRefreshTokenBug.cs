ed6dec8148cd447a80b11c86174010993f026c0d
Shervin-2005 <shervinshams40@gmail.com>
2026-08-20 12:53:44 +0330

    Add migration fixed referesh token issue

---
+++ Infastructure/Migrations/20260820091558_FixRefreshTokenBug.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRefreshTokenBug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId1",
+                table: "RefreshToken",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshToken_UserId1",
+                table: "RefreshToken",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshToken_Users_UserId1",
+                table: "RefreshToken",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshToken_Users_UserId1",
+                table: "RefreshToken");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshToken_UserId1",
+                table: "RefreshToken");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "RefreshToken");
+        }
+    }
+}
